fa7bc2c4534c64d9429640dc236f7494d79e7c84
son.xuan <sonnx.nguyen@gmail.com>
2016-07-29 19:31:51 +0700

    test

---
+++ app/src/main/java/eteam/android/com/testgit/MainActivity.java
@@ -0,0 +1,30 @@
+package eteam.android.com.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+}
